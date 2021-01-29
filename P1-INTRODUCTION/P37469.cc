44a8e3ca1dc0f3e75bf7613656f4acc7dd1aa21c
paucp1 <paucp1@users.noreply.github.com>
2021-01-29 15:32:58 +0100

    Create P37469.cc

---
+++ P1-INTRODUCTION/P37469.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+    
+    int h = n/3600;
+    int m = (n%3600)/60;
+    int s = n%60;
+    
+    cout << h << " " << m << " " << s << endl;
+}
