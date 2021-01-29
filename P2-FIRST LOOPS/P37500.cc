1239be8b3e5837fdfa3f098d233844363187ccd0
paucp1 <78021713+paucp1@users.noreply.github.com>
2021-01-29 15:58:37 +0100

    Create P37500.cc

---
+++ P2-FIRST LOOPS/P37500.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin >> n;
+    int x = 0;
+    
+    while (x <= n) {
+        cout << x << endl;
+        x = x + 1;
+    }
+}
