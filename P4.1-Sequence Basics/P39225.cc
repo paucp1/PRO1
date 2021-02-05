b6af04f71104137293918a4f23ebfdb2da6a9463
paucp1 <paucp1@users.noreply.github.com>
2021-02-05 12:58:19 +0100

    Create P39225.cc

---
+++ P4.1-Sequence Basics/P39225.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main () {
+     int n;
+     cin >> n;
+     int x;
+     
+     for (int i = 1; i <= n; ++i) {
+         cin >> x;
+     }
+     
+     cout << "A la posicio " << n << " hi ha un " << x << "." << endl;
+ }
