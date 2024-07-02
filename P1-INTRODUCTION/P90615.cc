ae17781bb8253149e12613aec94dee51f5da47e5
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:30:14 +0200

    Update P90615.cc

--- P1-INTRODUCTION/P90615.cc
+++ P1-INTRODUCTION/P90615.cc
@@ -1,4 +1,15 @@
 using namespace std;
+/*
+Feu un programa que llegeixi tres nombres i que n’escrigui el màxim. Aquest exercici és una mica més difícil que ‍.
+
+Entrada
+
+L’entrada consisteix en tres enters.
+
+Sortida
+
+Cal escriure una línia amb el màxim dels tres nombres.
+*/
 
 int main () {
     int x, y, z;
