f7966e813a904b53358096abf7c9589d5cc99a3c
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:36:56 +0200

    Update P34279.cc

--- P1-INTRODUCTION/P34279.cc
+++ P1-INTRODUCTION/P34279.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que afegeixi un segon a una hora del dia, donades les seves hores, minuts i segons.
+
+Entrada
+
+L’entrada consisteix en tres naturals h, m i s que representen una hora del dia, és a dir, tals que h<24, m<60 i s<60.
+
+Sortida
+
+Cal escriure el nou temps definit per h, m i s més un segon en el format “HH:MM:SS”.
+*/
+
 int main () {
     int h, m, s;
     cin >> h >> m >> s;
