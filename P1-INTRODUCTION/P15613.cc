200ab86273b8d5fdd166b5cdd322fed6afa448b5
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:31:40 +0200

    Update P15613.cc

--- P1-INTRODUCTION/P15613.cc
+++ P1-INTRODUCTION/P15613.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi un enter que representa una temperatura en graus Celsius, i que digui si fa calor, si fa fred, o si s’està bé. Suposeu que fa calor si la temperatura és més alta que 30 graus, que fa fred si és més baixa que 10 graus, i que s’està bé en altre cas. A més, cal avisar si amb temperatura donada l’aigua bulliria, o si es gelaria. Suposeu que l’aigua bull amb 100 o més graus, i que es gela amb 0 o menys graus.
+
+Entrada
+
+L’entrada consisteix en un enter.
+
+Sortida
+
+Cal escriure una línia dient si fa calor, si fa fred, o si s’està bé. Adicionalment, potser cal escriure una altra línia si l’aigua bulliria o si es gelaria.
+*/
+
 int main () {
     int x;
     cin >> x;
