4d800211d5bfec8b6941cae70e9f7d92494643cc
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:39:42 +0200

    Update P37297.cc

--- P1-INTRODUCTION/P37297.cc
+++ P1-INTRODUCTION/P37297.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi un natural i escrigui la suma de les seves tres darreres xifres.
+
+Entrada
+
+L’entrada és un natural n≥100.
+
+Sortida
+
+La sortida és una línia amb la suma de les tres darreres xifres de n.
+*/
+
 int main () {
     int x;
     cin >> x;
