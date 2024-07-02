422bf1e7d428d11fbfefed72a737b412510caf8d
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:41:39 +0200

    Update P64976.cc

--- P2-FIRST LOOPS/P64976.cc
+++ P2-FIRST LOOPS/P64976.cc
@@ -2,6 +2,20 @@
 #include <iostream>
 using namespace std;
 
+/*
+Les n primeres línies de la taula de multiplicar d’un natural n venen donades per n multiplicat per u, per dos, per tres, …, fins a n. Per exemple, per a n = 5 tenim 5× 1=5, 5× 2=10, 5× 3=15, 5× 4=20, 5× 5=25.
+
+Feu un programa que llegeixi un nombre natural n i escrigui les n primeres línies de la seva taula de multiplicar.
+
+Entrada
+
+L’entrada és un natural n.
+
+Sortida
+
+Cal escriure les n primeres línies de la taula de multiplicar de n, segons el format dels exemples.
+*/
+
 int main () {
     int n;
     cin >> n;
