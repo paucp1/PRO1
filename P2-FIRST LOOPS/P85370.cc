628780b3cd2ba72395777617ae843808dc7c4199
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:51:40 +0200

    Update P85370.cc

--- P2-FIRST LOOPS/P85370.cc
+++ P2-FIRST LOOPS/P85370.cc
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
+/*
+Donat un capital inicial c en euros, un interès anual i (expressat en %), un temps t en anys, i una indicació de si l’interès és simple o compost, calculeu en quants euros es transforma el capital inicial.
+
+Entrada
+
+L’entrada consisteix en dos nombres reals estrictament positius c i i, seguits d’un nombre enter estrictament positiu t, seguits de la paraula “simple” o la paraula “compost”.
+
+Sortida
+
+Escriviu el capital final amb 4 decimals.
+
+Observació
+
+Si programeu en C++, feu servir el tipus double, i poseu aquestes dues línies al principi del vostre main():
+
+    cout.setf(ios::fixed);
+    cout.precision(4);
+*/
+
 int main () {
     cout.setf(ios::fixed);
     cout.precision(4);
