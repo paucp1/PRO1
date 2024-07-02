019718e027084663e6376fccb8ba16ef1d767b6a
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:43:31 +0200

    Update P36668.cc

--- P2-FIRST LOOPS/P36668.cc
+++ P2-FIRST LOOPS/P36668.cc
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi un natural n, i que escrigui el resultat de la suma següent:
+
+    12 + 22 + … + (n−1)2 + n2 .
+Entrada
+
+L’entrada consisteix en un natural n.
+
+Sortida
+
+Escriviu la suma dels n primers quadrats: ∑i=1n i2.
+
+Observació
+
+Com que el sumatori té una fórmula tancada, no caldria fer cap bucle per resoldre aquest exercici.
+*/
+
 int main () {
     int n;
     cin >> n;
