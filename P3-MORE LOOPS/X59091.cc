9f72e5c079fea870d401fa41f04e6cb246e0c335
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:59:26 +0200

    Update X59091.cc

--- P3-MORE LOOPS/X59091.cc
+++ P3-MORE LOOPS/X59091.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que escrigui rectangles n× m. Escriviu cada rectangle de forma independent, i ompliu-lo amb un 9, désprés un 8, …, després un 0, etcètera.
+
+Entrada
+
+L’entrada consisteix en diversos naturals.
+
+Sortida
+
+Per a cada parell (n,m), escriviu un rectangle de mida n × m omplint-lo amb 9,8,…0, 9, …. Separeu dos quadrats amb una línea en blanc.
+*/
+
 int main () {
     int n, m;
     bool first = true;
