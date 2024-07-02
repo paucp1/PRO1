74b03e712d8a3142d63afa8b7a26ba577e030cc8
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:35:56 +0200

    Update P48107.cc

--- P1-INTRODUCTION/P48107.cc
+++ P1-INTRODUCTION/P48107.cc
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi dos naturals a i b, amb b > 0, i que escrigui la divisió entera d i el residu r de a entre b.
+
+Recordeu que, per definició, d i r han de ser els únics enters tals que 0 ≤ r < b i d · b + r = a.
+
+Entrada
+
+L’entrada conté a i b, amb b > 0.
+
+Sortida
+
+Escriviu una línia amb la divisió entera i el residu de a dividit per b, separats amb un espai.
+
+Observació
+
+Malgrat que les condicions sobre l’entrada d’aquest exercici impedeixen divisions per zero, proveu què li passa al vostre programa en aquest cas.
+*/
+
 int main () {
     int a, b;
     cin >> a >> b;
