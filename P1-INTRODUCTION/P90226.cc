720c93e1a2a668b0013d22be6c9ba52730997957
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:33:33 +0200

    Update P90226.cc

--- P1-INTRODUCTION/P90226.cc
+++ P1-INTRODUCTION/P90226.cc
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi dues paraules i que n’indiqui l’ordre lexicogràfic.
+
+Entrada
+
+L’entrada consisteix en dues paraules amb lletres minúscules a i b.
+
+Sortida
+
+Escriviu una línia indicant si a < b, a > b o a = b, seguint exactament el format dels exemples.
+
+Observació
+
+Si useu un llenguatge de programació modern, aquest exercici hauria de ser trivial.
+*/
+
 int main () {
     string a, b;
     cin >> a >> b;
