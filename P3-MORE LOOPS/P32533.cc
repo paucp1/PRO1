91d847a31cfaa3c18708679419dae4ac1115783d
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:55:54 +0200

    Update P32533.cc

--- P3-MORE LOOPS/P32533.cc
+++ P3-MORE LOOPS/P32533.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi un natural n i escrigui figures n× n com es mostra als exemples.
+
+Entrada
+
+L’entrada és un natural n ≥ 1.
+
+Sortida
+
+Cal escriure la figura corresponent.
+*/
+
 int main () {
     int n;
     cin >> n;
