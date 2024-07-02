075275c62307a546e3ce4396be377cd9b7806aed
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:45:43 +0200

    Update P50327.cc

--- P2-FIRST LOOPS/P50327.cc
+++ P2-FIRST LOOPS/P50327.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi un nombre i que l’escrigui del revés.
+
+Entrada
+
+L’entrada consisteix en un natural.
+
+Sortida
+
+Escriviu el número del revés, amb tants zeros a l’esquerra com té a la dreta.
+*/
+
 int main () {
     int n;
     cin >> n;
