a9368382301744033cc9818ca321c3d2bd85a393
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:45:16 +0200

    Update P74398.cc

--- P2-FIRST LOOPS/P74398.cc
+++ P2-FIRST LOOPS/P74398.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+El número 163 se representa como 10100011 en base 2, 20001 en base 3, 543 en base 8, 163 en base 10, y A3 en base 16. Dado un número, te pedimos que nos digas cuántas cifras son necesarias para representarlo en todas las bases de la 2 a la 16.
+
+Entrada
+
+Una entrada con un único número k, del 1 al 108.
+
+Salida
+
+Escribe 15 líneas de la forma “Base x: y cifras.”, donde x son los números del 2 al 16, y y es el número de cifras necesarias en base x para representar el número k. Escribe la salida exactamente igual como se te pide (espacios, punto final, etc.) o tu programa no será aceptado.
+*/
+
 int main () {
     int n;
     cin >> n;
