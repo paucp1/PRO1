d2cfb8fc95ce63fa8c825b8a8dce43c0cb8bbf9c
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:50:31 +0200

    Update P34451.cc

--- P2-FIRST LOOPS/P34451.cc
+++ P2-FIRST LOOPS/P34451.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi un enter estrictament positiu x i una seqüència d’enters, i que digui quants elements hi ha a la seqüència que siguin múltiples de x.
+
+Entrada
+
+L’entrada és un enter x > 0 seguit d’una seqüència d’enters.
+
+Sortida
+
+Escriviu el nombre d’elements de la seqüència que són múltiples de x.
+*/
+
 int main () {
     int x, n;
     cin >> x;
