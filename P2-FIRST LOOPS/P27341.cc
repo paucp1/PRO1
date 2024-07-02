650566414d7316487dedaaa6786988b68c76cc34
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:51:05 +0200

    Update P27341.cc

--- P2-FIRST LOOPS/P27341.cc
+++ P2-FIRST LOOPS/P27341.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que, per a cada parell d’enters a i b donats, calculi i escrigui la suma de tots els cubs entre a i b.
+
+Entrada
+
+L’entrada és una seqüència de parells d’enters a i b.
+
+Sortida
+
+Per a cada parell de l’entrada, cal escriure, en una línia i segons el format de l’exemple, el resultat de la suma a3 + (a + 1)3 + ⋯ + (b − 1)3 + b3.
+*/
+
 int main () {
     int a, b;
     
