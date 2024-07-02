3dc884217dfabd8851c0558885450bba052a1d2f
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:54:43 +0200

    Update X01646.cc

--- P3-MORE LOOPS/X01646.cc
+++ P3-MORE LOOPS/X01646.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi una base de numeració b i diversos nombres i que, per a cadascun, n’escrigui la suma en base 10 dels dígits de x en base b.
+
+Entrada
+
+L’entrada consisteix en el valor de b≥ 2 seguit d’una seqüència de naturals.
+
+Sortida
+
+Per a cada nombre, escriviu la suma (en base 10) dels seus digits en base b seguint el format dels exemples.
+*/
+
 int main () {
     int b, n;
     cin >> b;
