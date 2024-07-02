5d6080a8dd75ee3f92dfdc7cebad683054b9be66
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:44:02 +0200

    Update P59539.cc

--- P2-FIRST LOOPS/P59539.cc
+++ P2-FIRST LOOPS/P59539.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi un nombre n i que escrigui el nombre harmònic n-èsim, definit com Hn = 1/1 + 1/2 + ⋯ + 1/n.
+
+Entrada
+
+L’entrada consisteix en un natural n.
+
+Sortida
+
+Cal escriure Hn amb quatre xifres decimals.
+*/
+
 int main () {
     cout.precision(4);
     cout.setf(ios::fixed);
