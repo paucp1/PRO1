e4562475926d80b650003ccbe0e69ca1d6e3d332
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:41:08 +0200

    Update P31170.cc

--- P2-FIRST LOOPS/P31170.cc
+++ P2-FIRST LOOPS/P31170.cc
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+/*
+Feu un programa que llegeixi un nombre n i que escrigui la “taula de multiplicar” de n.
+
+Entrada
+
+L’entrada consisteix en un natural n entre 1 i 9.
+
+Sortida Cal escriure la “taula de multiplicar” de n.
+*/
 
 int main () {
     int n;
