4a5268cb9f6303edfa3f88fc47188900061224d8
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:37:58 +0200

    Update P61634.cc

--- P1-INTRODUCTION/P61634.cc
+++ P1-INTRODUCTION/P61634.cc
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que indiqui si un any és de traspàs o no.
+
+Un any de traspàs té 366 dies. Després de la reforma gregoriana, els anys de traspàs són els múltiples de quatre que no acaben en dos zeros, i també els acabats en dos zeros tals que el nombre que queda després de treure els dos zeros finals és divisible per quatre. Així, 1800 i 1900, tot i ser múltiples de quatre, no foren de traspàs; en canvi, 2000 ho va ser.
+
+Entrada
+
+L’entrada consisteix en un natural entre 1800 i 9999.
+
+Sortida
+
+Si l’any és de traspàs, cal escriure “YES”, altrament cal escriure “NO”.
+*/
+
 int main () {
     int n;
     cin >> n;
