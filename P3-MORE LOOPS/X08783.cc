7ef3698e0c16be80fa6a54cacf774a9aa5f8f97a
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:54:19 +0200

    Update X08783.cc

--- P3-MORE LOOPS/X08783.cc
+++ P3-MORE LOOPS/X08783.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que calculi el nombre de xifres en diverses bases.
+
+Entrada
+
+L’entrada consisteix en una seqüència de parells de naturals b≥ 2 i n≥ 1.
+
+Sortida
+
+Per a cada parell, escriviu el nobre de xifres de n en base b.
+*/
+
 int main () {
     int b, n;
     
