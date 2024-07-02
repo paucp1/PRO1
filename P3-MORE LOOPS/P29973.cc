790d2cf8de8b6d7d7c85f92da423a00eec00a9a2
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:55:12 +0200

    Update P29973.cc

--- P3-MORE LOOPS/P29973.cc
+++ P3-MORE LOOPS/P29973.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que, donat un nombre n, escrigui un “triangle de mida n”.
+
+Entrada
+
+L’entrada consisteix en un natural n.
+
+Sortida
+
+Escriviu n línies, de manera que la línia i contingui i asteriscos.
+*/
+
 int main () {
     int n;
     cin >> n;
