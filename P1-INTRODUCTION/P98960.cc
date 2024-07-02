9b65d87a0ee02a2a6addc37320a85eef41e7e047
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:32:21 +0200

    Update P98960.cc

--- P1-INTRODUCTION/P98960.cc
+++ P1-INTRODUCTION/P98960.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi una lletra i que l’escrigui en minúscula si era majúscula, o l’escrigui en majúscula si era minúscula.
+
+Entrada
+
+L’entrada consisteix en una lletra.
+
+Sortida
+
+Cal escriure una línia amb la lletra en minúscula si era majúscula, o en majúscula si era minúscula.
+*/
+
 int main () {
     char x;
     cin >> x;
