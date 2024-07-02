e7f3d2d2222995ee4d418c6b2cf4c4ae2d998cdb
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:42:48 +0200

    Update P97156.cc

--- P2-FIRST LOOPS/P97156.cc
+++ P2-FIRST LOOPS/P97156.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi dos nombres a i b, i que escrigui tots els nombres entre a i b.
+
+Entrada
+
+L’entrada consisteix en dos naturals a i b.
+
+Sortida
+
+Escriviu una línia amb a,a+1,…,b−1,b. Separeu els nombres amb comes.
+*/
+
 int main () {
     int a, b;
     cin >> a >> b;
