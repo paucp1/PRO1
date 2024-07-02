bf81222dda900a1edb77b1575f8fbfe5769d32f9
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:34:04 +0200

    Update P51126.cc

--- P1-INTRODUCTION/P51126.cc
+++ P1-INTRODUCTION/P51126.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que, donats dos intervals, en calculi l’interval corresponent a la intersecció o indiqui que aquesta és buida.
+
+Entrada
+
+L’entrada consisteix en quatre enters a1, b1, a2, b2 que representen els intervals [a1,b1] i [a2,b2]. Assumiu a1≤ b1 i a2≤ b2.
+
+Sortida
+
+Cal escriure “[]” si els intervals no tenen intersecció, o bé “[x,y]” si aquesta és la seva intersecció no buida.
+*/
+
 int main () {
     int a, b, c, d;
     cin >> a >> b >> c >> d;
