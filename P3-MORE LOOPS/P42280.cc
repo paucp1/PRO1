a47c36a5306549721f3a217eb3ccf2b3179492df
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:56:21 +0200

    Update P42280.cc

--- P3-MORE LOOPS/P42280.cc
+++ P3-MORE LOOPS/P42280.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Considereu un tauler d’escacs amb f files i c columnes, on a cada casella hi ha entre 0 i 9 monedes. Feu un programa que, donat un tauler, calculi el nombre total de monedes que conté.
+
+Entrada
+
+L’entrada comença amb el nombre de files f i el nombre de columnes c. Segueixen f línies, cadascuna amb c caràcters entre ‘0’ i ‘9’.
+
+Sortida
+
+Cal escriure el nombre total de monedes del tauler.
+*/
+
 int main () {
     int f, c;
     cin >> f >> c;
