d4d7c15fa55164322b73a3111e3c83ca41113eb3
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:50:04 +0200

    Update P38614.cc

--- P2-FIRST LOOPS/P38614.cc
+++ P2-FIRST LOOPS/P38614.cc
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+/*
+Un natural es diu txatxi si la suma dels seus dígits en posicions senars (començant a comptar per la dreta) és parell. Per exemple, 2 i 679031 són txatxis, però 357199 i 607 no ho són.
+
+Feu un programa que digui si un número donat és txatxi o no.
+
+Entrada
+
+L’entrada és un natural n.
+
+Sortida
+
+Escriviu en una línia si n és txatxi o no, segons el format dels exemples.
+*/
+
 int main () {
     int n;
     cin >> n;
