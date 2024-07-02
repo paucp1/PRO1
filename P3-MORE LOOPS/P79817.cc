6b5234949f1cb79efaae43e412c7ab83b9602079
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:53:22 +0200

    Update P79817.cc

--- P3-MORE LOOPS/P79817.cc
+++ P3-MORE LOOPS/P79817.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que calculi potències.
+
+Entrada
+
+L’entrada consisteix en diversos parells d’enters a i b. Assumiu b ≥ 0.
+
+Sortida
+
+Per a cada parell a,b, cal escriure ab. Suposeu, com és habitual, que 00 = 1.
+*/
+
 int main () {
     int a, b;
     
