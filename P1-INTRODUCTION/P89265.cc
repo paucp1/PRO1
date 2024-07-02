4692c3dd95762f0fdfd563475e63c1b007ca80db
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:35:25 +0200

    Update P89265.cc

--- P1-INTRODUCTION/P89265.cc
+++ P1-INTRODUCTION/P89265.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que, donats dos intervals, indiqui si un es troba dins de l’altre, i que calculi l’interval corresponent a la seva intersecció o indiqui que aquesta és buida.
+
+Entrada
+
+L’entrada consisteix en quatre enters a1, b1, a2, b2 que representen els intervals [a1,b1] i [a2,b2]. Assumiu a1≤ b1 i a2≤ b2.
+
+Sortida
+
+Cal escriure ‘=’ si els intervals són iguals, ‘1’ si el primer es troba dins del segon (però no són iguals), ‘2’ si el segon es troba dins del primer (però no són iguals), o bé ‘?’ altrament. També, cal escriure “[]” si els intervals no tenen intersecció, o bé “[x,y]” si aquesta és la seva intersecció no buida.
+*/
+
 int main () {
     int a, b, c, d;
     cin >> a >> b >> c >> d;
