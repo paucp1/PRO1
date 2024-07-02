57e063acd8e8279f6f84ccd9c2dd617841bf679d
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:34:50 +0200

    Update P56559.cc

--- P1-INTRODUCTION/P56559.cc
+++ P1-INTRODUCTION/P56559.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que, donats dos intervals, indiqui si un es troba dins de l’altre.
+
+Entrada
+
+L’entrada consisteix en quatre enters a1, b1, a2, b2 que representen els intervals [a1,b1] i [a2,b2]. Assumiu a1≤ b1 i a2≤ b2.
+
+Sortida
+
+Cal escriure ‘=’ si els intervals són iguals, ‘1’ si el primer es troba dins del segon (però no són iguals), ‘2’ si el segon es troba dins del primer (però no són iguals), o bé ‘?’ altrament.
+*/
+
 int main () {
     int a, b, c, d;
     cin >> a >> b >> c >> d;
