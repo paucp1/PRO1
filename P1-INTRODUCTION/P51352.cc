db2ab6a3cdc2421508d2bb427e4299389860acd2
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:38:29 +0200

    Update P51352.cc

--- P1-INTRODUCTION/P51352.cc
+++ P1-INTRODUCTION/P51352.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+El agua (A) vence a la piedra (P) que a su vez vence al viento (V) que vence al agua. Di quien gana el enfrentamiento, o si se produce un empate.
+
+Entrada
+
+Dos letras (A, P o V) separados por un espacio.
+
+Salida
+
+Escribe una línea con el número 1 si vence el primero, el número 2 si vence el segundo, o un guión (‘-’) si se produce un empate.
+*/
+
 int main () {
     char x, y;
     cin >> x >> y;
