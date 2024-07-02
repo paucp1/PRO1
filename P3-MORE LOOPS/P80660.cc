aa3093128c8ebed3fe6317b1481f8526d5cb50c8
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:58:44 +0200

    Update P80660.cc

--- P3-MORE LOOPS/P80660.cc
+++ P3-MORE LOOPS/P80660.cc
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+/*
+Sigui n qualsevol natural estrictament positiu. Considereu el procés següent: Si n és parell, dividiu-lo per dos. Altrament, multipliqueu-lo per 3 i sumeu-li 1. Quan arribeu a 1, pareu. Per exemple, començant en 3, s’obté la seqüència
+
+3, ‍ 10, ‍ 5, ‍ 16, ‍ 8, ‍ 4, ‍ 2, ‍ 1 .
+Des de l’any 1937 es conjectura que aquest procés acaba per a qualsevol n inicial, encara que no ho ha sabut demostrar ningú. En aquest problema no us en demanarem una demostració, només que feu un programa que escrigui quants passos triga a arribar a 1 cada n donada.
+
+Entrada
+
+L’entrada consisteix en diversos naturals n ≥ 1.
+
+Sortida
+
+Per a cada n, escriviu quants passos es triga a arribar a 1. Suposeu que aquest nombre està ben definit, és a dir, que la conjectura de l’enunciat és certa.
+*/
+
 int main () {
     int n;
     
