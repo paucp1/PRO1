63af9631a1dc4e73465a090497626dc8231ac53a
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:32:55 +0200

    Update P42042.cc

--- P1-INTRODUCTION/P42042.cc
+++ P1-INTRODUCTION/P42042.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi una lletra i que indiqui si és una majúscula, si és una minúscula, si és una vocal, i si és una consonant. Recordeu que en català les vocals són les lletres ‘a’, ‘e’, ‘i’, ‘o’ i ‘u’, i llurs majúscules.
+
+Entrada
+
+L’entrada consisteix en una lletra.
+
+Sortida
+
+Heu de dir si la lletra és majúscula, minúscula, vocal o consonant. Seguiu el format dels exemples.
+*/
+
 int main () {
     char x;
     cin >> x;
