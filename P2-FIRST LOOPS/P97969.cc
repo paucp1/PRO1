845bb79c1d456c2c3c60dadc791b64e555029ccf
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:48:12 +0200

    Update P97969.cc

--- P2-FIRST LOOPS/P97969.cc
+++ P2-FIRST LOOPS/P97969.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi una seqüència de caràcters acabada en punt i que escrigui quantes lletres ‘a’ conté.
+
+Entrada
+
+L’entrada consisteix en una seqüència de caràcters acabada en punt.
+
+Sortida
+
+Cal escriure el nombre de vegades que ‘a’ apareix a la seqüència.
+*/
+
 int main () {
     char x = 'x';
     int suma = 0;
