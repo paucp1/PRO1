5748f0ddaa4f73d4e54a0ea8fecc3bfb92d44ced
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:29:13 +0200

    Update X54725.cc

--- P1-INTRODUCTION/X54725.cc
+++ P1-INTRODUCTION/X54725.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi dos nombres i que n’escrigui el mínim.
+
+Entrada
+
+L’entrada consisteix en dos enters.
+
+Sortida
+
+Cal escriure una línia amb el mínim dels dos nombres.
+*/
+
 int main () {
     int x, y;
     cin >> x >> y;
