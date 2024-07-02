812d28b459251c91bdd120c19393ba55918f3b2d
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:47:10 +0200

    Update P39057.cc

--- P2-FIRST LOOPS/P39057.cc
+++ P2-FIRST LOOPS/P39057.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi diverses descripcions de rectangles i de cercles, i que per a cadascuna n’escrigui l’àrea corresponent.
+
+Entrada
+
+L’entrada comença amb un nombre n, seguit de n descripcions. Si és d’un rectangle, es té la paraula “rectangle” seguida de dos reals estrictament positius que n’indiquen la llargada i l’amplada. Si és d’un cercle, es té la paraula “cercle” seguida d’un real estrictament positiu que n’indica el radi.
+
+Sortida
+
+Per a cada descripció, escriviu-ne l’àrea amb 6 decimals.
+*/
+
 int main () {
     cout.precision(6);
     cout.setf(ios::fixed);
