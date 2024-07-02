bddae0d9646b4225bee7f5c158ae27d5d5074272
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:39:12 +0200

    Update P82374.cc

--- P1-INTRODUCTION/P82374.cc
+++ P1-INTRODUCTION/P82374.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi cinc enters x,a,b,c,d i digui si x∈[a,b]∪[c,d].
+
+Entrada
+
+L’entrada són cinc enters x,a,b,c,d.
+
+Sortida
+
+La sortida és una línia amb el text “si” si x∈[a,b]∪[c,d] i el text “no” altrament.
+*/
+
 int main () {
     int x, a, b, c, d;
     cin >> x >> a >> b >> c >> d;
