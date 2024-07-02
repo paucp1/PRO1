cacf2435c496f518925d4e598123b8ea2775b37c
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:58:11 +0200

    Update P58153.cc

--- P3-MORE LOOPS/P58153.cc
+++ P3-MORE LOOPS/P58153.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi parells de nombres n i m amb n ≥ m, i que per a cadascun escrigui Hn − Hm.
+
+Entrada
+
+L’entrada consisteix en diversos parells de naturals n i m tals que n ≥ m.
+
+Sortida
+
+Per a cada parell, cal escriure Hn − Hm amb deu xifres decimals.
+*/
+
 int main () {
     cout.precision(10);
     cout.setf(ios::fixed);
