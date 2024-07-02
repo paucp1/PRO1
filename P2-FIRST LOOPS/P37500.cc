6885f9022c37f38a6c380fa97634cbccffdaa8ca
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:40:38 +0200

    Update P37500.cc

--- P2-FIRST LOOPS/P37500.cc
+++ P2-FIRST LOOPS/P37500.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi un nombre n, i que escrigui tots els nombres entre 0 i n.
+
+Entrada
+
+L’entrada consisteix en un natural n.
+
+Sortida
+
+Escriviu en ordre tots els naturals entre 0 i n.
+*/
+
 int main () {
     int n;
     cin >> n;
