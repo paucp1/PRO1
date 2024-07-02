7e5bb4c382f4295ad9b2556f779a705a950060f1
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:56:47 +0200

    Update P13623.cc

--- P3-MORE LOOPS/P13623.cc
+++ P3-MORE LOOPS/P13623.cc
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+/*
+Considereu un tauler d’escacs amb f files i c columnes, on a cada casella hi ha entre 0 i 9 monedes. Suposeu que la casella de dalt a l’esquerra és blanca. Per exemple, aquest és un tauler buit amb quatre files i cinc columnes:
+
+showmover=false, label=false, maxfield=e4
+
+Feu un programa que, donat un tauler, calculi el nombre total de monedes contingudes a les caselles blanques.
+
+Entrada
+
+L’entrada comença amb el nombre de files f i el nombre de columnes c. Segueixen f línies, cadascuna amb c caràcters entre ‘0’ i ‘9’.
+
+Sortida
+
+Cal escriure el nombre total de monedes a les caselles blanques del tauler.
+*/
+
 int main () {
     int f, c;
     cin >> f >> c;
