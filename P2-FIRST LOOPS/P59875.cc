e38d894941284027d85387f8c891d3d92585b76a
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-02 10:42:11 +0200

    Update P59875.cc

--- P2-FIRST LOOPS/P59875.cc
+++ P2-FIRST LOOPS/P59875.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi dos nombres x i y, i que escrigui tots els nombres entre x i y (o entre y i x), de gran a petit.
+
+Entrada
+
+L’entrada consisteix en dos enters x i y.
+
+Sortida
+
+Cal escriure tots els enters entre x i y (o entre y i x), de gran a petit.
+*/
+
 int main () {
     int x, y;
     cin >> x >> y;
