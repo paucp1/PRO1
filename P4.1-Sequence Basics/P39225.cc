bb3bbf6186c9bb74dbf675e89799d3d25e02d758
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-03 10:29:04 +0200

    Update P39225.cc

--- P4.1-Sequence Basics/P39225.cc
+++ P4.1-Sequence Basics/P39225.cc
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
+
+/*
+Feu un programa que, donat un enter i i una seqüència de naturals x1, …, xn, escrigui xi.
+
+Entrada
+
+L’entrada comença amb un nombre i, seguit de la seqüència x1, …, xn acabada en −1. Teniu la garantia que 1 ≤ i ≤ n.
+
+Sortida
+
+Cal escriure el contingut de la posició i segons es mostra als exemples.
+*/
+
  int main () {
      int n;
      cin >> n;
