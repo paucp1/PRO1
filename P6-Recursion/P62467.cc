13ecdfd901b66edff74b972d04c31241496f9885
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-03 11:16:34 +0200

    Create P62467.cc

---
+++ P6-Recursion/P62467.cc
@@ -0,0 +1,31 @@
+/*
+Feu un programa que llegeixi un natural n i que escrigui 2n − 1 barres amb asteriscos seguint el patró que es pot deduir dels exemples.
+
+Entrada
+
+L’entrada consisteix en un natural n > 0.
+
+Sortida
+
+Escriviu 2n − 1 línies seguint el patró que es pot deduir dels exemples.
+*/
+
+   
+#include <iostream>
+using namespace std;
+
+void barres (int n) {
+    if ( n == 1) cout << "*" << endl;
+    else {
+        barres (n-1);
+        for (int i = 0; i != n; ++i) cout << "*";
+        cout << endl;
+        barres (n-1);
+    }
+}
+
+int main () {
+    int x;
+    cin >> x;
+    barres (x);
+}
