9d943b9b7385af524419c4ee8575349329a6c0de
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-03 10:27:59 +0200

    Update P89078.cc

--- P4.1-Sequence Basics/P89078.cc
+++ P4.1-Sequence Basics/P89078.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi una seqüència de naturals i que indiqui la posició del primer nombre parell.
+
+Entrada
+
+L’entrada consisteix en una seqüència de naturals que conté, com a mínim, un nombre parell.
+
+Sortida
+
+Cal escriure la posició del primer nombre parell de la seqüència.
+*/
+
 int main () {
     int n = -1;
     int pos = 0;
