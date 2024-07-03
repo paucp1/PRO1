d69aa024654a69195dbfc8fc6229f475efece003
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-03 10:28:35 +0200

    Update P32046.cc

--- P4.1-Sequence Basics/P32046.cc
+++ P4.1-Sequence Basics/P32046.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi una seqüència de naturals, i que compti i escrigui aquells que acabin amb les mateixes tres xifres que el primer nombre de la seqüència.
+
+Entrada
+
+L’entrada és una seqüència no buida de naturals més grans o iguals que 100.
+
+Sortida
+
+Cal escriure els nombres de l’entrada que acabin amb les mateixes tres xifres que el primer nombre de la seqüència, així com quants d’aquests nombres hi ha. Utilitzeu el format dels exemples.
+*/
+
 int main () {
     int n;
     int aux;
