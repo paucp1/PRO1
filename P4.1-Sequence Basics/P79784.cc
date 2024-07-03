75b5c5997c4b6e354858517cdab7baa0b74ea4ff
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-03 10:27:29 +0200

    Update P79784.cc

--- P4.1-Sequence Basics/P79784.cc
+++ P4.1-Sequence Basics/P79784.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi una seqüència de caràcters, cadascun dels quals codificant un moviment (‘n’ per nord, ‘s’ per sud, ‘e’ per est, i ‘o’ per oest), i que calculi la posició final d’un objecte que inicialment es trobés a la posició (0, 0). Suposeu que la primera component es correspon a la direcció est-oest, que anar cap a l’est significa sumar 1 a aquesta component, i que anar cap al sud significa sumar 1 a la segona component.
+
+Entrada
+
+L’entrada consisteix en una seqüència de caràcters ‘n’, ‘s’, ‘e’, o bé ‘o’.
+
+Sortida
+
+Cal escriure la posició final d’un objecte que inicialment es trobés a la posició (0, 0).
+*/
+
 int main () {
     char x;
     int eo = 0;
