c50b475cd6f4a759ec28281feb84e91ad34acd7b
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-03 10:29:59 +0200

    Update P48713.cc

--- P4.1-Sequence Basics/P48713.cc
+++ P4.1-Sequence Basics/P48713.cc
@@ -2,6 +2,22 @@
 #include <cmath>
 using namespace std;
 
+/*
+Feu un programa que llegeixi una seqüència de naturals i, per a cadascun, digui si és un nombre primer o no. Recordeu que un natural és primer si i només si és més gran que 1 i no té cap divisor positiu que no sigui 1 o ell mateix.
+
+Entrada
+
+L’entrada consisteix en un nombre n, seguit de n naturals.
+
+Sortida
+
+Per a cada natural donat, escriviu en una línia si és primer o no, seguint el format de l’exemple.
+
+Pista
+
+Per a cada nombre n, calen com a màxim uns √n  passos per saber si és primer o no.
+*/
+
 int main () {
     int n;
     cin >> n;
