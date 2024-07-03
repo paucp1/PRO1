8b2829617e2143805be7c2a318473c6464a3d6a1
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-03 10:27:00 +0200

    Update P75149.cc

--- P4.1-Sequence Basics/P75149.cc
+++ P4.1-Sequence Basics/P75149.cc
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+Feu un programa que llegeixi una seqüència de caràcters acabada en punt i que digui si té alguna ‘a’ minúscula o no.
+
+Entrada
+
+L’entrada consisteix en una seqüència de caràcters acabada en ‘.’.
+
+Sortida
+
+Cal escriure una línia amb “si” o “no”, depenent de si ‘a’ apareix a la seqüència o no.
+*/
+
 int main () {
     char x = 'x';
     bool trobada = false;
