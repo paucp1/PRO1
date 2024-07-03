de52e71058f9d1292fbe4834cb04faabbb06684d
paucp1 <78021713+paucp1@users.noreply.github.com>
2024-07-03 10:30:50 +0200

    Update P23001.cc

--- P4.1-Sequence Basics/P23001.cc
+++ P4.1-Sequence Basics/P23001.cc
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+/*
+Donada una seqüència de paraules, escriviu la longitud de la subseqüència consecutiva més llarga que només conté la primera paraula. És a dir, si la seqüència és s1, …, sn, cal escriure
+
+màx{j − i + 1 : 1 ≤ i ≤ j ≤ n ∧ si = si+1 = … = sj−1 = sj = s1 } .
+Entrada
+
+L’entrada consisteix en una seqüència no buida de paraules.
+
+Sortida
+
+Cal escriure el nombre de paraules de la subseqüència consecutiva més llarga que només conté la primera paraula.
+*/
+
 int main () {
     int lseq = 1;
     int max = 1;
